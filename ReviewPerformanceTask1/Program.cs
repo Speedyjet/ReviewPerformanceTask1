1a092a55da9e9564d5f0c7a5514c96021c19610b
Oleh Brusentsov <Oleh_Brusentsov@epam.com>
2023-05-27 19:46:46 +0200

    Comments implemented

--- ReviewPerformanceTask1/Program.cs
+++ ReviewPerformanceTask1/Program.cs
@@ -1,41 +1,35 @@
-﻿using System.Diagnostics;
-using System.Security.Cryptography;
-public class MyClass
+﻿using System.Security.Cryptography;
+
+internal class Program
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        MyClass c = new MyClass();
-        c.GeneratePasswordHashUsingSalt("hello", new byte[] { 1, 2, 3, 2, 3, 4, 3, 2, 3, 4, 3, 2, 3, 4, 4, 3, 2, 3, 3, 4, 4, 3, 2, 2, 3, 3 });
-    }
-    public string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
+        string? GeneratePasswordHashUsingSalt(string passwordText, byte[]? salt)
 
-    {
-        if (string.IsNullOrEmpty(passwordText) || salt == null)
         {
-            return null;
-        }
-        var iterate = 10000;
+            if (string.IsNullOrEmpty(passwordText) || salt == null || salt.Length < 16 )
+            {
+                return null;
+            }
+            var iterate = 10000;
 
-        using var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+            using var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
 
-        byte[] hash = pbkdf2.GetBytes(20);
+            var hash = pbkdf2.GetBytes(20);
 
-        byte[] hashBytes = new byte[36];
-        try
-        {
+            var hashBytes = new byte[36];
             Array.Copy(salt, 0, hashBytes, 0, 16);
-
             Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
         }
-        catch (Exception)
-        {
 
-            throw;
-        }
+        var result = GeneratePasswordHashUsingSalt("hello",
+            new byte[] { 1, 2, 3, 2, 3, 4, 3, 2, 3 });
 
-        var passwordHash = Convert.ToBase64String(hashBytes);
-
-        return passwordHash;
-
+        Console.WriteLine(result ?? "No result");
+        Console.ReadKey();
     }
 }
